6fa2adb03b98541bbbd41809418a2ed9c14dd76e
nagaame <sanweishuzu@gmail.com>
2023-03-11 22:43:07 +0800

    修改自定义信息

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+      avatar: 'https://avatars.githubusercontent.com/u/6214936',
+      name: 'nagaame',
+      description: 'My <a href="https://kuokuo.io" class="text-blue-500" target="_blank" >Blog</a> and <a href="https://github.com/nagaame" class="text-blue-500" target="_blank">Github</a>',
     },
   }
 }
